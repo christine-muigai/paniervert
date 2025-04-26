53bb3d356e25624e1d93aadd75d9b11d15bbc568
christine-muigai <git@github.com>
2025-04-26 18:19:28 +0300

    Auto commit

--- src/Checkout.jsx
+++
@@ -1,9 +0,0 @@
-export default function Checkout() {
-    return (
-      <div className="p-8">
-        <h1 className="text-4xl font-bold mb-4">Checkout</h1>
-        <p className="text-gray-600">Proceed with your order!</p>
-      </div>
-    );
-  }
-  
